4cc08f17ba21073819ecf234a34a3065337d8a56
theworm4002 <62223975+theworm4002@users.noreply.github.com>
2023-02-14 19:33:48 -0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,17 +2,25 @@ import os
 import time
 import argparse
 
+contentPath = '/proc/net/dev'
+# Find the os and get the right file path then run contentFile... or something. but for now this will do
+def contentFile(contentPath):
+    with open("/proc/net/dev") as f:
+        content = f.read()
+        return content
+
 parser = argparse.ArgumentParser(description='DDoS detector and network traffic capture script')
 parser.add_argument('-i', '--interface', type=str, help='The name of the network interface to monitor', required=False)
 parser.add_argument('-p', '--packet_threshold', type=int, help='The threshold for packets per second', required=False, default=10000)
 parser.add_argument('-d', '--dumpdir', type=str, help='The directory to save the network traffic capture', required=False, default='/tmp/')
 args = parser.parse_args()
 
+
+content = contentFile(contentPath)
+
 if args.interface:
     interface = args.interface
 else:
-    with open("/proc/net/dev") as f:
-        content = f.read()
     interfaces = [line.split(":")[0].strip() for line in content.splitlines()[2:]]
 
     interface = None
@@ -24,16 +32,17 @@ else:
 packet_threshold = args.packet_threshold
 dumpdir = args.dumpdir
 
+# Not opening the file twice should sve load and speed up script
+pkt_old = int(content.split(f"{interface}:")[1].split(" ")[2])
 while True:
-    with open("/proc/net/dev") as f:
-        content = f.read()
-    pkt_old = int(content.split(f"{interface}:")[1].split(" ")[2])
-    time.sleep(1)
-    with open("/proc/net/dev") as f:
-        content = f.read()
+    time.sleep(1)  
+      
+    content = contentFile(contentPath)
     pkt_new = int(content.split(f"{interface}:")[1].split(" ")[2])
     
     pkt = pkt_new - pkt_old
+    pkt_old = pkt_new
+
     print(f"\r{pkt} packets/s on interface {interface}", end="\033[0K")
     
     if pkt > packet_threshold:
